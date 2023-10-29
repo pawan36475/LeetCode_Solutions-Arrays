218edbc7193de5f71451bbaa57552e55fd6f6725
Pawan Deep Singh <82301521+pawan36475@users.noreply.github.com>
2023-10-29 21:41:07 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (10.37%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        String output=new String();
         char copy[]=new char[indices.length];
         for(int i=0;i<indices.length;i++){
             copy[indices[i]]=s.charAt(i);
